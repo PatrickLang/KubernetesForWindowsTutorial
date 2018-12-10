fd86a0314de0a9b501394c06cd458807cc173309
Patrick Lang <patrick.lang@hotmail.com>
2018-12-10 05:52:27 +0000

    Working on JSON creation

--- ClusterSetup/createAccounts.sh
+++ ClusterSetup/createAccounts.sh
@@ -1,7 +1,14 @@
 
 function createNamespaceAndUser {
-  cat serviceaccount.json | jq ".metadata.name"
-
+  ns=$1
+  echo Creating new namespace $ns
+  cat serviceaccount.json | jq ".metadata.name = \"$ns-user\"" | jq ".metadata.namespace = \"$ns\""
+  cat role.json | jq ".metadata.name = \"$ns-user-full-access\"" | jq ".metadata.namespace = \"$ns\""
+  cat binding.json | jq ".metadata.name = \"$ns-user-binding\"" | \
+      jq ".metadata.namespace = \"$ns\"" | \
+      jq ".subjects[0].name = \"$ns-user\"" | \
+      jq ".subjects[0].namespace = \"$ns\"" | \
+      jq ".roleRef.name = \"$ns-user-full-access\""
     # kubectl create -f access.yaml
     # set userToken=`kubectl describe sa mynamespace-user -n mynamespace`
     # kubectl get secret $userToken -n mynamespace -o "jsonpath={.data.token}" | base64 -D
