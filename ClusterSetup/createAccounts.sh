00fcd43289cdff9c5e31b868a990202d3e7b26fd
Patrick Lang <plang@microsoft.com>
2018-12-10 05:07:03 +0000

    Adding templates from tutorial

---
+++ ClusterSetup/createAccounts.sh
@@ -0,0 +1,9 @@
+
+function createNamespaceAndUser {
+    # kubectl create -f access.yaml
+    # set userToken=`kubectl describe sa mynamespace-user -n mynamespace`
+    # kubectl get secret $userToken -n mynamespace -o "jsonpath={.data.token}" | base64 -D
+    # kubectl get secret mynamespace-user-token-xxxxx -n mynamespace -o "jsonpath={.data['ca\.crt']}" | base64 -D
+    
+}
+
